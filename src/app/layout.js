33175aa9595917b374c0934544206b37bff4f634
KyawThetHtwe5502 <kyawthethtwe5502@gmail.com>
2025-01-21 13:05:46 +0630

    second commit

---
+++ src/app/layout.js
@@ -0,0 +1,15 @@
+
+
+const layout = ({children}) => {
+  return (
+    <html>
+        <body>
+            <main>
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default layout
